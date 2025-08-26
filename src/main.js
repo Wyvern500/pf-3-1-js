1fedbd4448361c6be7925deab94b82daa6433577
Wyvern500 <grassyclown@gmail.com>
2025-08-26 10:22:11 -0600

    Finished

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
-  //aqui va tu código
+export function addTwoNumbers(i1, i2){
+  console.log(i1 + i2);
+  return i1 + i2;
 }
+
+export function addThreeNumbers(i1, i2, i3){
+  console.log(i1 + i2 + i3);
+  return i1 + i2 + i3;
+}
+
+export function subTwoNumbers(i1, i2){
+  console.log(i1 - i2);
+  return i1 - i2;
+}
+
+addTwoNumbers(-10, 30);
